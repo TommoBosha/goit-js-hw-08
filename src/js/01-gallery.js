5d8270d5d73807f52180df347239420f23f4d143
TommoBosha <116563699+TommoBosha@users.noreply.github.com>
2023-01-11 10:58:28 +0200

    add

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,26 @@
-// Add imports above this line
-import { galleryItems } from './gallery-items';
-// Change code below this line
 
-console.log(galleryItems);
+import { galleryItems } from './gallery-items';
+
+import SimpleLightbox from "simplelightbox";
+
+import "simplelightbox/dist/simple-lightbox.min.css";
+// console.log(galleryItems);
+const galleryContainer = document.querySelector('.gallery');
+const imgsCardsMarkup = creatImgCardsMarkup(galleryItems);
+
+galleryContainer.insertAdjacentHTML('afterbegin', imgsCardsMarkup);
+
+function creatImgCardsMarkup(galleryItems) {
+    return galleryItems.map(({ preview, original, description }) => {
+        return `<a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>`;
+    })
+        .join('');
+}
+
+const lightbox = new SimpleLightbox('.gallery a', {
+    captionDelay: 250,
+    captionType: "alt",
+    captionsData: "alt",
+}); 
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,37 @@
+import throttle from 'lodash.throttle';
+const ref = {
+    form: document.querySelector('.feedback-form'),
+    input: document.querySelector('.feedback-form input'),
+    textarea: document.querySelector('.feedback-form textarea'),
+};
+const STORAGE_KEY = 'feedback-form-state';
 
+ref.form.addEventListener('input', throttle(onInputChange, 500));
+ref.form.addEventListener('submit', onFormSubmit);
+
+saveData();
+let data = {};
+
+function onInputChange() {
+    data.email = ref.input.value;
+    data.message = ref.textarea.value;
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(data));
+}
+
+function saveData() {
+    const savedData = localStorage.getItem(STORAGE_KEY);
+    const parsedData = JSON.parse(savedData);
+    if (savedData) {
+        ref.input.value = parsedData.email;
+        ref.textarea.value = parsedData.message;
+        return parsedData;
+    }
+}
+
+function onFormSubmit(event) {
+    event.preventDefault();
+    console.log(saveData());
+
+    event.target.reset();
+    localStorage.removeItem(STORAGE_KEY);
+}
